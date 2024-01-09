10abf1576c98b5e8aa7fc6517ce9fa78da39ea89
Bryn Glotfelty <150275734+Bryncidently@users.noreply.github.com>
2024-01-09 15:48:26 -0500

    Update connector.js

--- db/connector.js
+++ db/connector.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   port: '3306',
   user: 'root',
-  password: 'K6733910y!',
+  password: 'password',
   database: 'employee_db',
 });
 
